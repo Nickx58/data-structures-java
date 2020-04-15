b4656f0c5c234386ec9dca9f119cb7875ab1b472
nikhil8_sharma <nikhilx58@gmail.com>
2020-04-15 22:46:21 +0530

    CodeChef: Practice-> SUM OR Diff

---
+++ codechef/Practice/Sum OR Difference/Submission.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n , m;
+    cin>>n>>m;
+    if(n>m) {
+        cout<<n - m<<'\n';
+    } else {
+        cout<<n+m<<'\n';
+    }
+    return 0;
+}
