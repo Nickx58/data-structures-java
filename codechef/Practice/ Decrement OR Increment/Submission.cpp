bc62dff26a35524a1764d581dee2fc131426492f
nikhil8_sharma <nikhilx58@gmail.com>
2020-04-15 22:43:47 +0530

    CodeChef: Practice-> DEC OR INC

---
+++ codechef/Practice/ Decrement OR Increment/Submission.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;cin>>n;
+	if(n%4 == 0) {
+	    n += 1;
+	} else {
+	    n -= 1;
+	}
+	cout<<n<<'\n';
+	return 0;
+}
