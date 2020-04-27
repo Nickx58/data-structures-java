9bafebec40ef46d628262d13178c07e259055f28
nikhil8_sharma <nikhilx58@gmail.com>
2020-04-27 20:41:50 +0530

    DS: Linked List Delete Beginning

---
+++ Data_Structure/Linked_List/Delete_At_Beginning.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *next;
+};
+
+Node *head;
+
+void push(int data) {
+    Node *temp = new Node();
+    temp->data = data;
+    temp->next = head;
+    head = temp;
+}
+
+void print() {
+    Node *temp = head;
+    while (temp != NULL) {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void deleteStart() {
+    Node *temp = head->next;;
+    head = temp;
+    delete temp;
+}
+
+int main() {
+    head = NULL;
+    push(5);
+    push(4);
+    push(3);
+    push(2);
+    push(1);
+    print();
+    cout<<"Deleting First Node"<<endl;
+    deleteStart();
+    print();
+    return 0;
+}
