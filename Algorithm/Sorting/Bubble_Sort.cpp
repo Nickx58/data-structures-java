bd6c617cf8bed90afbda1aee389566d8831dc778
nikhil8_sharma <nikhilx58@gmail.com>
2020-05-03 22:46:43 +0530

    Algo: Bubble Sort Algo fixed

--- Algorithm/Sorting/Bubble_Sort.cpp
+++ Algorithm/Sorting/Bubble_Sort.cpp
@@ -1,20 +1,17 @@
-// Bubble Sort efficent Implementation
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void print(int arr[], int size) {
-    for(int i = 0; i<size; i++) {
-        cout<<arr[i]<<" ";
-    }
-    cout<<'\n';
-}
-
-void bubble(int arr[], int size, bool isSorted) {
-    while(!isSorted) {
+void bubbleSort(int arr[], int size)
+{
+    bool isSorted = false;
+    while (!isSorted)
+    {
         isSorted = true;
-        for(int i = 0; i<size; i++) {
-            if(arr[i] > arr[i+1]) {
-                swap(arr[i], arr[i+1]);
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                swap(arr[i], arr[i + 1]);
                 isSorted = false;
             }
         }
@@ -22,13 +19,19 @@ void bubble(int arr[], int size, bool isSorted) {
     }
 }
 
-int main() {
-    int arr[] = {1,3,2,5,7,6,4,10,8,9};
-    int size = sizeof(arr)/sizeof(arr[0]);
-    int lastSorted = size - 1;
-    bool isSorted = false;
+void print(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {4, 1, 3, 7, 8, 2, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, size);
     print(arr, size);
-    bubble(arr,lastSorted,isSorted);
-    print(arr, size);
-    return 0;
 }
