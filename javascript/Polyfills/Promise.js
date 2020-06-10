e5aa38af91ae0996f6557d13300e5d99048719f8
Nickx58 <nikhilx58@gmail.com>
2020-06-10 16:49:02 +0530

    Promise Polyfil

---
+++ javascript/Polyfills/Promise.js
@@ -0,0 +1,48 @@
+function allPromise(promises) {
+  // return a new promise
+  return new Promise((resolve, reject) => {
+    // check promises count
+    var promiseCount = promises.length;
+    var promisedData = [];
+    var settledCount = 0;
+
+    // check the status
+
+    promises.forEach((promise, i) => {
+      promise
+        .then((data) => {
+          // pushed in the array and increment count
+          promisedData.push(data);
+          settledCount++;
+          // check the promise count and settledCount
+          if (settledCount === promiseCount) {
+            resolve(promisedData);
+          }
+        })
+        .catch((error) => {
+          // if promise fail then reject
+          reject(error);
+        });
+    });
+  });
+}
+
+function fn1() {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(10);
+    }, 5000);
+  });
+}
+
+function fn2() {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(20);
+    }, 1000);
+  });
+}
+
+allPromise([fn1(), fn2()]).then((response) => {
+  console.log(response);
+});
