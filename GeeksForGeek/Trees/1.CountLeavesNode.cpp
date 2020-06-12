f464a918d454500c0d68f725b0f8362281fe346e
Nickx58 <nikhilx58@gmail.com>
2020-06-12 22:17:15 +0530

    GeeksForGeeks count leaves node

---
+++ GeeksForGeek/Trees/1.CountLeavesNode.cpp
@@ -0,0 +1,31 @@
+//User function Template for C++
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+/* Should return count of leaves. For example, return
+    value should be 2 for following tree.
+         10
+      /      \ 
+   20       30 */
+int countLeaves(Node *root)
+{
+  // Your code here
+  int x;
+  int y;
+  if (root == NULL)
+    return 0;
+  x = countLeaves(root->left);
+  y = countLeaves(root->right);
+  if (root->left == NULL && root->right == NULL)
+  {
+    return 1;
+  }
+  return x + y;
+}
