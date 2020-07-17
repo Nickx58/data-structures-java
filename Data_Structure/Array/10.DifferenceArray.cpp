ebe9b624df114de9daa21a5ecdf69dd13f286f60
Nickx58 <nikhilx58@gmail.com>
2020-07-17 22:50:56 +0530

    DS: Difference Array

---
+++ Data_Structure/Array/10.DifferenceArray.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int c[2];
+
+void display(int arr[], int size)
+{
+  int i;
+  for (i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+void differenceArray(int a[], int b[], int aa, int bb)
+{
+  int i, j, k;
+  i = j = k = 0;
+  while (i < aa && j < bb)
+  {
+    if (a[i] < b[j])
+    {
+      c[k++] = a[i++];
+    }
+    else if (b[j] < a[i])
+    {
+      j++;
+    }
+    else
+    {
+      i++;
+      j++;
+    }
+  }
+  while (i < aa)
+  {
+    c[k++] = a[i++];
+  }
+}
+
+int main()
+{
+  int A[] = {2, 4, 6, 8, 10};
+  int B[] = {1, 3, 4, 5, 7, 9, 10};
+  int sizeA = sizeof(A) / sizeof(A[0]);
+  int sizeB = sizeof(B) / sizeof(B[0]);
+  differenceArray(A, B, sizeA, sizeB);
+  int sizeC = sizeof(c) / sizeof(c[0]);
+  display(c, sizeC);
+  return 0;
+}
