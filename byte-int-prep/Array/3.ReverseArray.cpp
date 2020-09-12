ca2046adf55d962ea38fcbdd62d6f38dc449e795
Nickx58 <nikhilx58@gmail.com>
2020-09-12 11:54:32 +0530

    Byte-INT: Array Reverse

---
+++ byte-int-prep/Array/3.ReverseArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+  int start = 0;
+  int end = size - 1;
+  while (start < end)
+  {
+    swap(arr[start], arr[end]);
+    start++;
+    end--;
+  }
+}
+
+void print(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  int arr[] = {1, 2, 3, 4, 5, 6};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  reverseArray(arr, size);
+  print(arr, size);
+  return 0;
+}
